79dd13fe3e42029edcab3340e48312405b07faa6
Nallagatla Manikanta <manikant@adobe.com>
2020-05-12 19:10:08 +0530

    Add a dummy service code

---
+++ Backend/kwue/src/main/java/com/iitr/kwue/KwueApplication.java
@@ -0,0 +1,13 @@
+package com.iitr.kwue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwueApplication.class, args);
+	}
+
+}
---
+++ Backend/kwue/src/test/java/com/iitr/kwue/KwueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitr.kwue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
