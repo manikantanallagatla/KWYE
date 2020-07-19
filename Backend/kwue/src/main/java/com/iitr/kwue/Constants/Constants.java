606384b750ffb1da0332c2c82ca3b665093f38c5
bandakhi <bandakhi@amazon.com>
2020-07-19 16:07:12 +0530

    Adding Slf4j dependency and creating constants for recurrent used variables

--- Backend/kwue/src/main/java/com/iitr/kwue/Constants/Constants.java
+++ Backend/kwue/src/main/java/com/iitr/kwue/Constants/Constants.java
@@ -2,4 +2,26 @@ package com.iitr.kwue.Constants;
 
 public class Constants {
 	public static final String UTC_ZONE_ID = "UTC";
+	
+	public static class TableConstants {
+		public static class OneTimePasswordConstants {
+			public static final String PHONE_NO = "phone_no";
+			public static final String OTP_NUMBER = "otpNumber";
+			public static final String TIME_STAMP = "timeStamp";
+		}
+		
+		public static class UserConstants {
+			public static final String PHONE_NO = "phone_no";
+			public static final String USER_NAME = "user_name";
+		}
+		
+		public static class AddressConstants {
+			public static final String PHONE_NO = "phoneNo";
+			public static final String STREET = "street";
+			public static final String PINCODE = "pinCode";
+			public static final String STATE = "state";
+			public static final String CITY = "city";
+			public static final String VERSION = "version";
+		}
+	}
 }
